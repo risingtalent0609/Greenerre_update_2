5baeedb90e9949b373f451987e1ec4e99cab7bd0
risingtalent0609 <bsbussiness000@gmail.com>
2022-11-02 15:47:23 +0900

    Modal fixed

--- src/components/BookModal/BookModal.js
+++ src/components/BookModal/BookModal.js
@@ -1,41 +1,71 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 
 import DateTimePicker from "react-datetime-picker";
 import "./BookModal.scss";
 
 const BookModal = (props) => {
+  const formRef = useRef(null);
+
+  const scriptUrl =
+    "https://script.google.com/macros/s/AKfycbw1YiviawtNFHzZ_hARkEEqZFION8lWiTBqSw7TfYhwrCQX__s-hUIqZ2fh_xwRlt_q/exec";
+
   const [date, setDate] = useState(new Date());
 
   const handleClose = () => {
     props.onClose();
   };
 
-  const handleBook = () => {
-    props.onBook();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(scriptUrl, { method: "POST", body: new FormData(formRef.current) })
+      .then((res) => {
+        alert("Your Book Consultation Confirmed!");
+        props.onClose();
+        console.log("BOOK CONSULTATION SUCCESSFULLY SUBMITTED");
+      })
+      .catch((err) => console.log(err));
   };
+
   return (
     <Modal show={props.show} onHide={handleClose} size="lg">
       <Modal.Header closeButton>
         <Modal.Title>Book a consultation</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
+        <Form method="post" ref={formRef} onSubmit={handleSubmit}>
           <Form.Group className="mb-3">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Name"></Form.Control>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Name"
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Organisation</Form.Label>
-            <Form.Control type="text" placeholder="Organisation"></Form.Control>
+            <Form.Control
+              type="text"
+              name="organisation"
+              placeholder="Organisation"
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Position</Form.Label>
-            <Form.Control type="text" placeholder="Position"></Form.Control>
+            <Form.Control
+              type="text"
+              name="position"
+              placeholder="Position"
+            ></Form.Control>
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Email"></Form.Control>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Email"
+            ></Form.Control>
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -44,21 +74,20 @@ const BookModal = (props) => {
             <Form.Label>DateTime</Form.Label>
             <DateTimePicker
               value={date}
+              name="date"
               onChange={setDate}
               className="form-control"
               minDate={new Date()}
             />
           </Form.Group>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" type="submit">
+            Book
+          </Button>
         </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleBook}>
-          Book
-        </Button>
-      </Modal.Footer>
     </Modal>
   );
 };
